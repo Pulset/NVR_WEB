c63be8ded3e5951af297370304bb50975a81b395
pulse <745653239@qq.com>
2018-12-02 22:39:41 +0800

    环境的搭建

---
+++ nvr_web/src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <div id="app">
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+export default {
+  name: 'app',
+  components: {
+    // HelloWorld
+  }
+}
+</script>
+
+<style>
+
+</style>
